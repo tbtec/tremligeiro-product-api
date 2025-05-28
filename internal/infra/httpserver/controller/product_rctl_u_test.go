5da07613c45c3559378f70289b6e01d3d8b93f7d
Jacqueline Morais <jacqueline.morais@nubank.com.br>
2025-05-28 11:12:06 -0300

    Unit Test

--- internal/infra/httpserver/controller/product_rctl_u_test.go
+++ internal/infra/httpserver/controller/product_rctl_u_test.go
@@ -1,11 +1,18 @@
 package controller
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/tbtec/tremligeiro/internal/core/domain/usecase"
+	"github.com/tbtec/tremligeiro/internal/infra/container"
+	"github.com/tbtec/tremligeiro/internal/infra/database/model"
+	"github.com/tbtec/tremligeiro/internal/infra/httpserver"
+	"github.com/tbtec/tremligeiro/test/repository"
 )
 
 func TestBuildProductUpdateResponse(t *testing.T) {
@@ -31,4 +38,38 @@ func TestBuildProductUpdateResponse(t *testing.T) {
 	assert.Equal(t, output.CategoryName, resp.Category.CategoryName)
 	assert.Equal(t, output.CreatedAt, resp.CreatedAt)
 	assert.Equal(t, output.UpdatedAt, resp.UpdatedAt)
+}
+
+func TestProductUpdateController_Handle_ExecuteError(t *testing.T) {
+
+	container := &container.Container{
+
+		ProductRepository: &repository.MockProductRepo{
+
+			FindOneFunc: func(ctx context.Context, id string) (*model.Product, error) {
+				return nil, errors.New("not found")
+			},
+		},
+	}
+	ctrl := NewProductUpdateByIdController(container)
+
+	input := ProductUpdateRequest{
+		Name:        "Produto Teste",
+		Description: "Descrição",
+		CategoryId:  1,
+		Amount:      10.0,
+	}
+	inputBytes, _ := json.Marshal(input)
+	req := httpserver.Request{
+		Params: map[string]string{"productId": "123"},
+		Body:   inputBytes,
+	}
+
+	resp := ctrl.Handle(context.Background(), req)
+
+	assert.NotEqual(t, 200, resp.Code)
+	// Ajuste conforme o tipo retornado por HandleError
+	errMsg, ok := resp.Body.(httpserver.ErrorMessage)
+	assert.True(t, ok)
+	assert.Contains(t, errMsg.Error.Description, "Internal Server Error")
 }
